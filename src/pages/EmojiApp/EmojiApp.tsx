287a6a2a1c86c57530e77fb81117c74e4f330bc0
magrega <proshanov@yandex.ru>
2023-04-26 15:37:14 +0300

    deleted react calls from emojiapp.tsx

--- src/pages/EmojiApp/EmojiApp.tsx
+++ src/pages/EmojiApp/EmojiApp.tsx
@@ -1,4 +1,4 @@
-import { useState, FC } from 'react';
+import { useState, FC, ChangeEvent } from 'react';
 import EmojiContainer from './Emoji/EmojiContainer/EmojiContainer';
 import EmojiLimit from './Emoji/EmojiLimit/EmojiLimit';
 import Form from './Emoji/Form/Form';
@@ -8,12 +8,12 @@ const EmojiApp: FC = () => {
   const [formValue, setFormValue] = useState('');
   const [limit, setLimit] = useState<number>(15);
 
-  const onFormChange = (e: React.ChangeEvent<{ value: string }>) => {
+  const onFormChange = (e: ChangeEvent<{ value: string }>) => {
     const search = e.target.value;
     setFormValue(search);
   }
 
-  const getSelectedDropdownValue = (e: React.ChangeEvent<{ value: string }>) => {
+  const getSelectedDropdownValue = (e: ChangeEvent<{ value: string }>) => {
     setLimit(Number(e.target.value))
   }
 
